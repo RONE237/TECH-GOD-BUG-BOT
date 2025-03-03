dccfb8a7bbd1ecb7c9922b299f83620db8ab940f
RONE237 <ronesteve42@gmail.com>
2025-03-03 20:47:32 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,13 +11,13 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernomer = "237696315415"
-global.ownername = "🐛Tech God"
+global.ownername = "RONE237"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
 global.ownernumber = '237696315415'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownername = 'RONE237' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
